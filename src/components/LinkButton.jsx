e0744654d41ffda968cd5aef0cc982c69b51e21e
Thanh Huy Tu <thtu1@uci.edu>
2024-05-24 23:52:30 -0700

    Update Home Page

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -2,5 +2,5 @@ import { Button } from "@material-tailwind/react";
 import { Link, Outlet } from "react-router-dom";
 
 export function LinkButton(props) {
-  return <Link to={props.destination}><Button>{props.name}</Button></Link>;
+  return <Link to={props.destination}><Button className="!bg-yellow-400 !text-blue-800">{props.name}</Button></Link>;
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,19 +3,18 @@ import { IDInput } from "../components/IDInput"
 import { SearchBar } from "../components/SearchBar"
 export default function Home () {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-start w-screen">
-            <SearchBar></SearchBar>
-            <div id="bg-container" className="relative h-auto bg-contain bg-no-repeat w-full">
-                <img id="bg" src="../../home_bg.svg"></img>
+        <div className="min-h-screen flex flex-col items-center justify-start w-screen max-w-full">
+            <div id="bg-container" className="relative h-max bg-contain bg-no-repeat w-screen max-w-full">
+                <img id="bg" src="../../home_bg.svg" className="max-w-full z-0"></img>
                 <div className="box-border w-full absolute top-1/2 -translate-y-1/2 flex items-center justify-around">
                     <div id="greeting" className=""> 
-                        <h1 className="text-5xl text-white">Welcome to CulinaryCompass!</h1>
+                        <h1 className="text-5xl text-white mb-2">Welcome to CulinaryCompass!</h1>
                         <h2 className="text-2xl text-white">Start planning your meals at UCI and beyond!</h2>
                     </div>
                     <img src="../../home_img.png" className="h-auto w-48"></img>
                 </div>
             </div>
-            <Button>Get Started</Button>
+            <Button className="!bg-yellow-400 !text-blue-800 !text-3xl -translate-y-3/4">Get Started</Button>
             <div id="row-1-container" className="flex">
                 <div id="row-1-text" className="relative w-fit p-2">
                     <p className="absolute bg-red-300 top-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto libero impedit id porro cum iste velit, corporis, quasi omnis eligendi accusantium, quia illo? Repellendus, officia accusantium inventore sapiente voluptatum nihil?</p>
