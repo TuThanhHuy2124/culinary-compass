8461df6b46e2b73308081125f8d823c502e668a7
Chanmonyneath Has <chanmoh@uci.edu>
2024-05-24 22:40:56 -0700

    Fixed typo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,16 +9,16 @@ export default function Layout () {
         <>
             <header className="w-screen h-max flex items-center justify-between">
                 <h1 id="header-title" className="text-lg text-blue-800">CulinaryCompass</h1>
-                <ul class="flex space-x-4">
+                <ul className="flex space-x-4">
                     <LinkButton name="Home" destination="/"></LinkButton>
                     <li><IDInput></IDInput></li>
                 </ul>
             </header>
             <Outlet/>
             <footer className="w-screen h-max flex items-center justify-between">
-                <a href="https://github.com/TuThanhHuy2124/culinary-compass"><Button>GitHub</Button></a>
+                <LinkButton name="GitHub" destination="https://github.com/TuThanhHuy2124/culinary-compass"></LinkButton>
                 <h1 id="footer-title" className="text-lg text-blue-800">CulinaryCompass</h1>
-                <a href="mailto:thtu1@uci.edu"><Button>Contact Us</Button></a>
+                <LinkButton name="Contact Us" destination="mailto:thtu1@uci.edu"></LinkButton>
             </footer>
         </>
     )
