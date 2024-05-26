d2c12fd30bb87dae0bec2df430280382fd143cca
Thanh Huy Tu <tuthanhhuy2004@gmail.com>
2024-05-26 04:26:24 -0700

    Remove "public" in URL

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,8 @@ export default withMT({
   theme: {
     extend: {
       backgroundImage: {
-        'circle-pattern': "url('./public/backgrounds/circle_bg.svg')",
-        'cool-pattern' : "url('./public/backgrounds/Group 56.png')"
+        'circle-pattern': "url('./backgrounds/circle_bg.svg')",
+        'cool-pattern' : "url('./backgrounds/middle_wave.png')"
       }
     },
   },
